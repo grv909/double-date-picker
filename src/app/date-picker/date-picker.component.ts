001051d8dd20c46fe167e7313fec599b397e85e0
gaurav <gaurav.909.iitr@gmail.com>
2025-06-21 22:16:09 +0530

    feat(date-picker):Added Calendar view

--- src/app/date-picker/date-picker.component.ts
+++ src/app/date-picker/date-picker.component.ts
@@ -1,66 +1,78 @@
-import { Component } from '@angular/core';
+import { JsonPipe, NgFor } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-date-picker',
   standalone: true,
-  imports: [],
+  imports: [NgFor, JsonPipe],
   templateUrl: './date-picker.component.html',
-  styleUrl: './date-picker.component.scss'
+  styleUrl: './date-picker.component.scss',
 })
 export class DatePickerComponent {
+  // returns todays date -> Sun Jun 15 2025 18:15:44 GMT+0530 (India Standard Time)
+  today = new Date();
 
- // returns todays date -> Sun Jun 15 2025 18:15:44 GMT+0530 (India Standard Time)
-today = new Date();
+  baseMonth = signal([
+    new Date(
+      this.today.getFullYear(),
+      this.today.getMonth(),
+      this.today.getDate()
+    ),
+    new Date(
+      this.today.getFullYear(),
+      this.today.getMonth() + 1,
+      this.today.getDate()
+    ),
+  ]);
 
+  months = computed(() => [
+    this.generateCalendar(this.baseMonth()[0]),
+    this.generateCalendar(this.baseMonth()[1]),
+  ]);
 
+  generateCalendar(startDate: Date): {
+    monthLabel: string;
+    days: { date: Date; offset: number | null }[];
+  } {
+    //gives year in YYYY format as integer
+    const year = startDate.getFullYear();
 
+    // return month number but 0 index based , for June -> 05
+    const month = startDate.getMonth();
 
-generateCalendar(startDate: Date){
+    // keeping day=0 will give last day of month-1
+    const firstDay = new Date(year, month, 1);
 
-//gives year in YYYY format as integer
-  const year = startDate.getFullYear();
+    const lastDay = new Date(year, month + 1, 0);
 
-  
-  
+    const days: { date: Date; offset: number | null }[] = [];
 
-// return month number but 0 index based , for June -> 05
-  const month = startDate.getMonth();
+    // Use of setDate
+    //   const date = new Date('2023-05-15');
+    // date.setDate(20);
+    // console.log(date); // May 20, 2023
 
-  // keeping day=0 will give last day of month-1
-  const firstDay = new Date(year,month,1);
+    for (
+      let i = new Date(firstDay);
+      i <= new Date(lastDay);
+      i.setDate(i.getDate() + 1)
+    ) {
+      days.push({
+        date: new Date(i.getFullYear(), i.getMonth(), i.getDate()),
+        offset: i.getDate() === 1 ? i.getDay() + 1 : null,
+      });
+    }
 
-  const lastDay = new Date(year,month+1,0);
+    const monthName = startDate.toLocaleDateString('en-US', { month: 'long' });
+    const tempYear = startDate.toLocaleDateString('en-US', { year: 'numeric' });
 
-  const days :Date[]= [];
-
-// Use of setDate
-//   const date = new Date('2023-05-15');
-// date.setDate(20);
-// console.log(date); // May 20, 2023
-
-  for(let i = new Date(firstDay);i<= new Date(lastDay);i.setDate(i.getDate()+1)){
-   
-    days.push(new Date(i.getFullYear(),i.getMonth(),i.getDate()));
-    
-
-    
+    return {
+      monthLabel: `${monthName} ${tempYear}`,
+      days,
+    };
   }
-  console.log(days);
-  
-  
-  
-  
-  
-
-  
-
-
-
-}
-checkConsole(){
- this.generateCalendar(this.today);
-  
-  
-}
-
+  checkConsole() {
+    console.log(this.generateCalendar(this.today));
+    console.log(this.months());
+  }
 }
